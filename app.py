4b76010c7f6fcd119ab098c55fdbd6a0df8ce608
VENKY <venkynalla.az@outlook.com>
2025-03-25 23:34:15 -0400

    Enabled CORS.

--- app.py
+++ app.py
@@ -1,63 +1,37 @@
-import pyodbc
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
-from collections import OrderedDict
-
-# from azure.identity import ClientSecretCredential
 from azure.identity import ManagedIdentityCredential
 from azure.keyvault.secrets import SecretClient
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 CORS(app)
 
+# Get database credentials from Azure Key Vault
+key_vault_uri = os.getenv("AZURE_KEY_VAULT_URI")
+credential = ManagedIdentityCredential()
+secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
 
-def get_db_connection():
-    # client_id = os.getenv("AZURE_CLIENT_ID")
-    # tenant_id = os.getenv("AZURE_TENANT_ID")
-    # client_secret = os.getenv("AZURE_CLIENT_SECRET")
-    key_vault_uri = os.getenv("AZURE_KEY_VAULT_URI")
-    # credential = ClientSecretCredential(tenant_id, client_id, client_secret)
-    credential = ManagedIdentityCredential()
+server = secret_client.get_secret("MSSQL-DB-SERVER").value
+database = secret_client.get_secret("MSSQL-DB-NAME").value
+username = secret_client.get_secret("MSSQL-DB-USER").value
+password = secret_client.get_secret("MSSQL-DB-PASSWORD").value
 
-    secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
+# SQLAlchemy connection string
+DATABASE_URL = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
 
-    server = secret_client.get_secret("MSSQL-DB-SERVER")
-    database = secret_client.get_secret("MSSQL-DB-NAME")
-    username = secret_client.get_secret("MSSQL-DB-USER")
-    password = secret_client.get_secret("MSSQL-DB-PASSWORD")
-
-    connection_string = (
-        f'DRIVER={{ODBC Driver 17 for SQL Server}};'
-        f'SERVER={server.value};'
-        f'DATABASE={database.value};'
-        f'UID={username.value};'
-        f'PWD={password.value};'
-    )
-
-    return pyodbc.connect(connection_string)
+# Create SQLAlchemy engine with connection pooling
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=5)
 
 
 @app.route("/employees", methods=["GET"])
 def fetch_data():
+    """Fetches employee data using a connection from the pool."""
     try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT TOP 10 id, first_name, last_name, email FROM employee")
-
-        rows = cursor.fetchall()
-        data = [
-            {
-                "id": row[0],
-                "firstName": row[1],
-                "lastName": row[2],
-                "email": row[3]
-            }
-            for row in rows
-        ]
-
-        cursor.close()
-        conn.close()
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT TOP 10 id, first_name, last_name, email FROM employee"))
+            data = [{"id": row[0], "firstName": row[1], "lastName": row[2], "email": row[3]} for row in result]
 
         return jsonify(data)
     except Exception as e:
